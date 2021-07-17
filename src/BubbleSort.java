d9c23c4b13bb193e196ec12e6f62518626c6f71d
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-17 16:14:32 +0530

    bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort {
+
+    public void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; ++i) {
+            boolean flag = true;
+            for (int j = i + 1; j < arr.length; ++j) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    flag = false;
+                }
+            }
+            if (flag) break;
+        }
+    }
+}
